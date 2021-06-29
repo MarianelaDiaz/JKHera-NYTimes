717474e37088705a2e155684e010041950707af0
Tomás <dieztomasjoaquin@gmail.com>
2021-06-28 23:28:53 -0300

    Remove source and isLocallyStored properties from Article

--- src/main/java/NYTimesAPI/entities/ArticleData.kt
+++ src/main/java/NYTimesAPI/entities/ArticleData.kt
@@ -2,24 +2,18 @@ package NYTimesAPI
 
 interface ArticleData {
     val description: String
-    val source: Int
     val articleURL: String
-    val isLocallyStored: Boolean
     val sourceLogoURL: String
 }
 
 data class NYTArticleData(
     override val description: String,
-    override val source: Int = 1,
     override val articleURL: String,
-    override val isLocallyStored: Boolean = false,
     override val sourceLogoURL: String = ""
 ): ArticleData
 
 object EmptyCard: ArticleData {
     override val description: String = ""
-    override val source: Int = 1
     override val articleURL: String = ""
-    override val isLocallyStored: Boolean = false
     override val sourceLogoURL: String = ""
 }
